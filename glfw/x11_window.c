b71b34427e53697f0425fd069ad0c69d137af676
Marten Trip <marten143@protonmail.com>
2020-06-23 17:20:29 +0200

    Patch out 'NewKeyboardNotify' events for broken Anne Pro keyboard

--- glfw/x11_window.c
+++ glfw/x11_window.c
@@ -1177,11 +1177,13 @@ static void processEvent(XEvent *event)
     {
         XkbEvent *kb_event = (XkbEvent*)event;
         switch(kb_event->any.xkb_type) {
+            /* My broken Anne Pro keyboard keeps sending XkbNewKeyboardNotify
+             * events...
             case XkbNewKeyboardNotify: {
                 int32_t old_id = _glfw.x11.xkb.keyboard_device_id;
                 if (!glfw_xkb_update_x11_keyboard_id(&_glfw.x11.xkb)) return;
                 if (old_id != _glfw.x11.xkb.keyboard_device_id) keymap_dirty = true;
-            }
+            }*/
                 /* fallthrough */
             case XkbMapNotify:
             {
